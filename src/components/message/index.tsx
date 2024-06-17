a2752cb5baa86bc511dbf49d9411746db6bf9394
MatheusRisatti <risattimatheus@gmail.com>
2024-06-17 09:39:01 -0300

    syle: inicia a estilização da seção de mensagens

--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -5,7 +5,16 @@ export function Message(){
         <>
         <div className="message">
             <div className="message-container">
-                
+                <div className="message-content">
+                    <h1 className='message-title'>Mande sua mensagem para os Noivos</h1>
+                    <form action="">
+                        <div className="message-header">
+                            <input type="text" placeholder='Nome' id='name' />
+                            <input type="text" placeholder='Email' id='email' />
+                        </div>
+                            <input type="text" placeholder='Digite sua messagem aqui' id='message' />
+                    </form>
+                </div>
             </div>
         </div>
         </>
