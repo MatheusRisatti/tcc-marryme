27ef2d33a2a240fb87f6a097141c8cc924d9b55b
MatheusRisatti <risattimatheus@gmail.com>
2024-11-27 02:03:46 -0300

    feat: adiciona sistema de rotas por meio de react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,23 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import { App } from './template1/Template';
+import { Template1 } from './pages/template1/';
+import { Template2 } from './pages/template2/';
+import { Menu } from './pages/menu';
 
-const router = createBrowserRouter([
+export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <Menu/>,
   },
+  {
+    path: "/template1",
+    element: <Template1/>,
+  },
+  {
+    path: "/template2",
+    element: <Template2/>,
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
